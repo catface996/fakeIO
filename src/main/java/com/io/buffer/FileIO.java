8c3b9cac40c715d3ccb5ad2fa367dfe9bb681ad0
catface <zlyxzq@163.com>
2021-06-16 11:38:32 +0800

    [新增功能](main): 区分jvm堆上的缓冲区和java进程堆上的缓冲区.

--- src/main/java/com/io/buffer/FileIO.java
+++ src/main/java/com/io/buffer/FileIO.java
@@ -32,6 +32,52 @@ public class FileIO {
         }
     }
 
+    /**
+     * 分配jvm堆上的缓冲区
+     */
+    @Test
+    public void allocateJvmHeapBuffer() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024 * 1024);
+        log.info("init buffer:{}", buffer);
+        buffer.put(context);
+        log.info("after put buffer:{}", buffer);
+        buffer.flip();
+        log.info("flip buffer:{}", buffer);
+        for (int i = 0; i < 5; i++) {
+            byte b = buffer.get();
+            log.info("get byte:{}", (char)b);
+            log.info("get buffer:{}", buffer);
+        }
+        buffer.compact();
+        log.info("compact buffer:{}", buffer);
+        for (int i = 0; i < 5; i++) {
+            buffer.put((byte)('o' + i));
+            log.info("put buffer:{}", buffer);
+        }
+    }
 
+    /**
+     * 分配java进程怼上的缓冲区,此堆是内核对进程分配的堆空间,非jvm堆空间
+     */
+    @Test
+    public void allocateProgressHeapBuffer() {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024 * 1024 * 1024);
+        log.info("init buffer:{}", buffer);
+        buffer.put(context);
+        log.info("after put buffer:{}", buffer);
+        buffer.flip();
+        log.info("flip buffer:{}", buffer);
+        for (int i = 0; i < 5; i++) {
+            byte b = buffer.get();
+            log.info("get byte:{}", (char)b);
+            log.info("get buffer:{}", buffer);
+        }
+        buffer.compact();
+        log.info("compact buffer:{}", buffer);
+        for (int i = 0; i < 5; i++) {
+            buffer.put((byte)('o' + i));
+            log.info("put buffer:{}", buffer);
+        }
+    }
 
 }
